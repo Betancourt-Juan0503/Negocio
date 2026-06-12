42da7bf06af0e3a48b93665f4782ce8163ced4bc
Betancourt-Juan0503 <a.24308051280503@cbtis128.edu.mx>
2026-06-12 07:39:20 -0600

    Fix formatting and error handling in control_ingresos

--- control_ingresos.py
+++ control_ingresos.py
@@ -1,16 +1,16 @@
 from flask import Blueprint, request, jsonify
-from database import db  # Importa la conexión a Atlas desde tu database.py
+from database import db 
 
 Control_ingresos_bp = Blueprint('control_ingresos', __name__)
-coleccion = db['control_ingresos']  # Colección en MongoDB
+coleccion = db['control_ingresos'] 
 
 @Control_ingresos_bp.route('/api/control_ingresos', methods=['POST'])
 def registrar_ingreso():
     try:
         datos = request.json
-        print("📥 Datos recibidos en Control de Ingresos:", datos)
+        print(" Datos recibidos en Control de Ingresos:", datos)
         
-        # Guarda en la base de datos
+    
         coleccion.insert_one(datos.copy())
         return jsonify({"message": "Ingreso guardado exitosamente"}), 201
     except Exception as e:
