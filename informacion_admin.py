ff27fd69248364270da1818f56fb8e66946d5670
Betancourt-Juan0503 <a.24308051280503@cbtis128.edu.mx>
2026-06-12 07:40:06 -0600

    Refactor registrar_admin function for clarity

--- informacion_admin.py
+++ informacion_admin.py
@@ -2,15 +2,15 @@ from flask import Blueprint, request, jsonify
 from database import db
 
 informacion_admin_bp = Blueprint('informacion_admin', __name__)
-coleccion = db['informacion_admin']  # Colección en MongoDB
+coleccion = db['informacion_admin'] 
 
 @informacion_admin_bp.route('/api/informacion_admin', methods=['POST'])
 def registrar_admin():
     try:
         datos = request.json
-        print("📥 Datos recibidos en Información Admin:", datos)
+        print(" Datos recibidos en Información Admin:", datos)
         
-        # Guarda en la base de datos
+ 
         coleccion.insert_one(datos.copy())
         return jsonify({"message": "Información administrativa guardada exitosamente"}), 201
     except Exception as e:
