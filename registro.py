4c686f18fcc502986f1c3b02cd82d3ef5418291b
Betancourt-Juan0503 <a.24308051280503@cbtis128.edu.mx>
2026-06-12 07:40:27 -0600

    Update registro.py

--- registro.py
+++ registro.py
@@ -2,15 +2,14 @@ from flask import Blueprint, request, jsonify
 from database import db
 
 Registro_bp = Blueprint('registro', __name__)
-coleccion = db['registros']  # Colección en MongoDB
+coleccion = db['registros']
 
 @Registro_bp.route('/api/registro', methods=['POST'])
 def registrar_usuario():
     try:
         datos = request.json
-        print("📥 Datos recibidos en Registro:", datos)
-        
-        # Guarda en la base de datos
+        print(" Datos recibidos en Registro:", datos)
+     
         coleccion.insert_one(datos.copy())
         return jsonify({"message": "Registro completado exitosamente"}), 201
     except Exception as e:
